6e9168a5e6c784dc68d450926932399bba7c160b
Igor Dyniewski <igor.dyniewski@gmail.com>
2020-10-27 02:12:19 +0100

    Update BoxSelect.js

--- src/components/BoxSelect.js
+++ src/components/BoxSelect.js
@@ -15,7 +15,7 @@ const Wrapper = styled.div`
 `
 const MainRectangle = styled.div`
     border: 3px solid ${(props) => (props.isActive ? '#1BFEC0' : '#dadaed')};
-    transition: border 100ms ease-in;
+    transition: border 200ms ease-in;
     width: 50px;
     height: 50px;
     display: flex;
@@ -24,7 +24,7 @@ const MainRectangle = styled.div`
 `
 const SmallRectangle = styled.div`
     border: 3px solid ${(props) => (props.isActive ? '#1BFEC0' : '#dadaed')};
-    transition: border 100ms ease-in;
+    transition: border 200ms ease-in;
     width: 15px;
     height: 15px;
     display: flex;
@@ -49,6 +49,7 @@ const ActiveSmallestRectangle = styled.div`
     height: 3px;
     background-color: #1bfec0;
     opacity: ${(props) => (props.isActive ? 1 : 0)};
+    transition: opacity 200ms ease;
 `
 
 const BoxSelect = (props) => {
